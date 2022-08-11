e16b179c07b1dcca135ebfe271b17ed07772cedb
AnastasiaShalukhina <exachka4@gmail.com>
2022-08-11 16:38:25 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
+  minNum;
+  maxNum;
+  gNumber;
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minNum = min;
+    this.maxNum = max;
+  }
 
-    }
+  guess() {
+    this.gNumber = Math.ceil(this.minNum + (this.maxNum - this.minNum) / 2);
+    return this.gNumber;
+  }
 
-    guess() {
+  lower() {
+    return (this.maxNum = this.gNumber);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return (this.minNum = this.gNumber);
+  }
 }
 
 module.exports = GuessingGame;
